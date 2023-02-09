a16207d25a7c950e72fab97e43179aec9d9211a5
vivu96 <sharmavivu96@gmail.com>
2023-02-09 20:56:57 +0530

    first

---
+++ demo.java
@@ -0,0 +1,16 @@
+public class demo
+{
+
+public static void main(String args[]){
+
+System.out.println("hello   ok.....");
+System.out.println("hello   ok.....");
+System.out.println("hello   ok.....");
+
+
+
+}
+
+
+
+}
